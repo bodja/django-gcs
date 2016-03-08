e473248643a44667a07fad78af22f852ab88b0de
Bogdan Radko <bodja.rules@gmail.com>
2016-03-08 12:13:34 +0100

    Update details

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup
 
 setup(
     name='django-gcs',
+    packages=['django_gcs'],
     version='0.1',
     description='Django file storage backend for Google Cloud Storage',
     author='Bogdan Radko',
     author_email='bodja.rules@gmail.com',
-    packages=[
-        'django_gcs'
-    ],
     install_requires=[
         'gcloud == 0.11.0'
     ],
     license='MIT',
-    url='https://github.com/bodja/django-gcs'
+    url='https://github.com/bodja/django-gcs',
+    download_url='https://github.com/bodja/django-gcs/tarball/0.1',
+    keywords=['django', 'storage', 'gcs', 'google cloud storage'],
+    classifiers=[]
 )
